c42c5b6747184a01214a82de63d27f22818a8cec
Scubaboy <robin@robincawsey.plus.com>
2015-02-21 20:29:44 +0000

    initial git checkin

---
+++ QuadMbedWebApp/App/Factories/ToolOptions/toolOptionsFactory.js
@@ -0,0 +1,38 @@
+﻿(function () {
+    'use strict'
+
+    var factoryId = "toolOptionsFct";
+
+    angular.module('app').factory(factoryId, [toolOptionsFct]);
+
+    function toolOptionsFct() {
+
+        function tools() {
+            return [
+                {
+                    name: "Settings",
+                    image: "../../Content/images/Settings-Icon-3.png"
+                },
+                {
+                    name: "Kpi",
+                    image: "../../Content/images/Settings-Icon-3.png"
+                },
+                {
+                    name: "Kpi",
+                    image: "../../Content/images/Settings-Icon-3.png"
+                },
+                {
+                    name: "Kpi",
+                    image: "../../Content/images/Settings-Icon-3.png"
+                }
+            ];
+        };
+
+        var services =
+            {
+                tools: tools
+            };
+
+        return services;
+    }
+})()
---
+++ QuadMbedWebApp/App/Header/header.js
@@ -0,0 +1,17 @@
+﻿(function () {
+    'use srrict'
+
+    var controllerId = 'headerCtrl';
+
+    angular.module('app').controller(controllerId, ['toolOptionsFct', headerCtrl]);
+
+    function headerCtrl(toolOptionsFct) {
+        var vm = this;
+
+        vm.toolOptions = toolOptionsFct.tools();
+
+        vm.click = function (item) {
+            console.log("click");
+        }
+    };
+})()

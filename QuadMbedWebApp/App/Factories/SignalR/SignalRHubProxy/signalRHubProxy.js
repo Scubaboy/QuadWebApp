43169a09691b6dd3b6c6f2d35288ce08087bab0d
Scubaboy <robin@robincawsey.plus.com>
2015-03-01 20:01:35 +0000

    Started to add signalR support to javascript.

---
+++ QuadMbedWebApp/App/Factories/SignalR/SignalRHubProxy/signalRHubProxy.js
@@ -0,0 +1,71 @@
+﻿(function () {
+    'use strict'
+
+    var factoryId = 'signalRHubProxy'
+    anguar.module('app').facory(factoryId, [signalRHubProxy]);
+
+    function signalRHubProxy() {
+        function hubProxy(hubProxy, qDefer) {
+            var hubProxyInternal = hubProxy;
+            var hubActionQueue = [];
+            var that = this;
+
+            function createCallbackFunc(actionName, callback) {
+                return function (data) {
+                    if (hubActionQueue.length) {
+                        var currentRequest = hubActionQueue.shift();
+                        callback(data, currentRequest.defer);
+
+                        if (hubActionQueue.length) {
+                            var nextRequest = hubActionQueue.shift();
+                            hubProxyInternal.proxy.invoke(nextRequest.hubActionName, nextRequest.parameters);
+                        }
+                    }
+                };
+            }
+
+            this.setProxyHubActions = function(hubActions) {
+                hubActions.forEach(function (hubAction) {
+                    hubProxyInternal.proxy.on(hubAction.actionname, createCallbackFunc(hubAction.actionname, hubAction.callback)); //createCallbackFunc(hubAction.actionname, hubAction.callback)
+                });
+            }
+
+            this.forceActionRequestCycle = function () {
+                if (hubActionQueue.length) {
+                    hubProxyInternal.proxy.invoke(hubActionQueue[0].hubActionName, hubActionQueue[0].parameters);
+                }
+            }
+
+            this.callHubActionRequest = function (hubActionName, parameters) {
+                var requestDefer = qDefer.defer();
+
+                if (!hubProxyInternal.hubReady) {
+                    hubActionQueue.push({
+                        defer: requestDefer,
+                        parameters: parameters,
+                        hubActionName: hubActionName
+                    });
+                } else {
+                    hubActionQueue.push({
+                        defer: requestDefer,
+                        parameters: parameters,
+                        hubActionName: hubActionName
+                    });
+                    if (hubActionQueue.length === 1) {
+                        if (parameters != undefined) {
+                            hubProxyInternal.proxy.invoke(hubActionName, parameters);
+                        } else {
+                            hubProxyInternal.proxy.invoke(hubActionName);
+                        }
+                    }
+                }
+
+                return requestDefer.promise;
+            }
+        }
+
+        return {
+            hubProxy: hubProxy
+        };
+    }
+})()
---
+++ QuadMbedWebApp/App/Services/SignalRHub/signalRHub.js
@@ -0,0 +1,60 @@
+﻿(function () {
+    'use strict';
+
+    var serviceId = "signalRHub";
+
+    angular.module('app').service(serviceId, ['$', SignalRHub]);
+
+    function SignalRHub($) {
+
+            var connection = null;
+            var hubReady = false;
+            var hubProxys = [];
+            var that = this;
+
+            this.isHubReady = function() {
+                return hubReady;
+            }
+
+            this.initialise = function(hubs) {
+                //Getting the connection object
+                connection = $.hubConnection();
+
+                //Creating proxys
+                hubs.forEach(function (item) {
+                    hubProxys.push({
+                        proxy: connection.createHubProxy(item.hubName),
+                        proxyName: item.hubName,
+                        connStartedAction: item.connStartedAction,
+                        hubReady: false
+                    });
+                });
+            }
+
+            this.start = function() {
+                connection.start().then(function () {
+                    hubReady = true;
+
+                    //Update hubproxy array with hub ready flags
+                    hubProxys.forEach(function (item) {
+                        item.hubReady = true;
+                        item.connStartedAction();
+                    });
+
+                    console.log("signalr service started.");
+                });
+            }
+
+            this.getHubProxyEntity = function(hubName) {
+                var hubSearch = $.grep(hubProxys, function (hubItem) {
+                    return hubItem.proxyName === hubName;
+                });
+
+                if (hubSearch.length == 1) {
+                    return hubSearch[0];
+                } else {
+                    return undefined;
+                }
+            }
+    }
+})()

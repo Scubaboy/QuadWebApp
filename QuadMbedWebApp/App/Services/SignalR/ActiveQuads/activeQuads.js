d9e1f22e4eb9fe9275d7c258308c832175f84da2
Scubaboy <robin@robincawsey.plus.com>
2015-03-11 15:53:41 +0000

    added missing untracked change

---
+++ QuadMbedWebApp/App/Services/SignalR/ActiveQuads/activeQuads.js
@@ -0,0 +1,42 @@
+﻿(function () {
+    'use strict'
+
+    var factoryId = 'ActiveQuadsSigRFct'
+
+    angular.module('app').service(factoryId, ['$q', 'signalRHub', 'signalRHubProxy', activeQuadsSigRFct]);
+
+    function activeQuadsSigRFct($q, signalRHub, signalRHubProxy) {
+
+            var configProxy = null;
+            var that = this;
+            var hubActions = []; 
+
+            function activeQuadsUpdated (data, defer) {
+                console.log(data);
+                defer.resolve(data);
+            }
+
+            this.initialise = function() {
+                configProxy = new signalRHubProxy.hubProxy(signalRHub.getHubProxyEntity("ActiveQuadHub"), $q);
+
+                hubActions.push({
+                    actionname: 'UpdateActiveQuads',
+                    callback: activeQuadsUpdated,
+                });
+
+                configProxy.setProxyHubActions(hubActions);
+            }
+
+            this.hubReadyNotify = function() {
+                configProxy.forceActionRequestCycle();
+            }
+
+            this.getActiveQuads = function() {
+                return configProxy.callHubActionRequest('GetActiveQuads');
+            }
+
+            this.tryTakeQuad = function(quad) {
+                return configProxy.callHubActionRequest('TryTakeQuad', quad);
+            }
+        };
+})()
